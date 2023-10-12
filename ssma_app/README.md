861b71fa711b9e787b120c5c7ad0ff29886b173c
Ernest Langan <ErnestLasher57@gmail.com>
2023-10-12 17:01:59 -0600

    Initial Flutter project creation

---
+++ ssma_app/README.md
@@ -0,0 +1,3 @@
+# ssma_app
+
+A new Flutter project.
---
+++ ssma_app/android/app/src/main/kotlin/com/example/ssma_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ssma_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
